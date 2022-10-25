29470a91dd9a6461a45bf3b3503e8c1ef4a85e3c
DasonTio <codingdec14@gmail.com>
2022-10-25 12:05:55 +0700

    frontend 5x

--- SERVER_MODULE/frontend/src/views/detail-form.vue
+++ SERVER_MODULE/frontend/src/views/detail-form.vue
@@ -1,16 +1,17 @@
 <template>
-  <body>
+  <body v-if="form">
     <nav class="navbar navbar-expand-lg sticky-top bg-primary navbar-dark">
       <div class="container">
-        <a class="navbar-brand" href="manage-forms.html">Formify</a>
+        <router-link class="navbar-brand" :to="{ name: 'manageForm' }"
+          >Formify</router-link
+        >
+        <!-- <a class="navbar-brand" href="manage-forms.html">Formify</a> -->
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link active" href="#">Administrator</a>
           </li>
           <li class="nav-item">
-            <a href="index.html" class="btn bg-white text-primary ms-4"
-              >Logout</a
-            >
+            <a @click="logout" class="btn bg-white text-primary ms-4">Logout</a>
           </li>
         </ul>
       </div>
@@ -19,14 +20,18 @@
     <main>
       <div class="hero py-5 bg-light">
         <div class="container text-center">
-          <h2 class="mb-2">Biodata - Web Tech Members</h2>
-          <div class="text-muted mb-4">To save web tech members biodata</div>
+          <h2 class="mb-2">{{ form.name }}</h2>
+          <div class="text-muted mb-4">{{ form.description }}</div>
           <div>
             <div>
               <small>For user domains</small>
             </div>
             <small
-              ><span class="text-primary">webtech.id, webtech.org</span></small
+              ><span class="text-primary"
+                ><span v-for="(a_domain, i) in form.allowed_domains" :key="i">{{
+                  a_domain.domain + ", "
+                }}</span></span
+              ></small
             >
           </div>
         </div>
@@ -41,25 +46,23 @@
                   type="text"
                   class="form-control form-link"
                   readonly
-                  value="http://localhost:8080/forms/biodata"
+                  :value="url"
                 />
                 <a href="submit-form.html" class="btn btn-primary">Copy</a>
               </div>
 
               <ul class="nav nav-tabs mb-2 justify-content-center">
                 <li class="nav-item">
-                  <a class="nav-link active" href="detail-form.html"
-                    >Questions</a
-                  >
+                  <a class="nav-link active" href="#">Questions</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="responses.html">Responses</a>
+                  <a class="nav-link" href="#">Responses</a>
                 </li>
               </ul>
             </div>
           </div>
 
-          <div class="row justify-content-center">
+          <div class="row justify-content-center" v-for="question in questions">
             <div class="col-lg-5 col-md-6">
               <div class="question-item card card-default my-4">
                 <div class="card-body">
@@ -209,7 +212,11 @@ Male,Female,Others</textarea
                 </div>
               </div>
 
-              <div class="question-item card card-default my-4">
+              <div
+                class="question-item card card-default my-4"
+                v-for="(question, i) in questions"
+                :key="i"
+              >
                 <div class="card-body">
                   <div class="form-group my-3">
                     <input
@@ -247,7 +254,11 @@ Male,Female,Others</textarea
                     >
                   </div>
                   <div class="mt-3">
-                    <button type="submit" class="btn btn-outline-danger">
+                    <button
+                      type="submit"
+                      class="btn btn-outline-danger"
+                      @click="remove(question.id)"
+                    >
                       Remove
                     </button>
                   </div>
@@ -366,32 +377,66 @@ import { API_URL } from "@/constant";
 import axios from "axios";
 
 export default {
-  name: "detailForm",  
-  data(){
-   return{
-      slug: this.$route.params
-   }  
+  name: "detailForm",
+  data() {
+    return {
+      url: location.origin + `/forms/${this.$route.query.slug}`,
+      slug: this.$route.query.slug,
+      form: null,
+      questions: null,
+    };
   },
   methods: {
+    remove(e) {
+      axios
+        .delete(API_URL + `/${this.slug}/questions/${e}`)
+        .then((res) => {
+          axios
+            .get(API_URL + "/forms/" + slug)
+            .then((res) => {
+              console.log(res.data.form);
+              this.form = res.data.form;
+              this.questions = res.data.form.questions;
+            })
+            .catch((err) => {});
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
     submitForm(e) {
       const form = new FormData(e.target);
       const data = Object.fromEntries(form.entries());
-   
-      axios.post(API_URL + "");
+
+      axios
+        .post(API_URL + `/${this.slug}/questions`, data)
+        .then((res) => {
+          alert(res.data.message);
+          axios
+            .get(API_URL + "/forms/" + slug)
+            .then((res) => {
+              console.log(res.data.form);
+              this.form = res.data.form;
+              this.questions = res.data.form.questions;
+            })
+            .catch((err) => {});
+        })
+        .catch((err) => {});
     },
   },
   created() {
-   console.log(this.$route.query)
+    const slug = this.$route.query.slug;
     axios.defaults.headers.common["Authorization"] =
       "Bearer " + localStorage.token;
-   
+
     axios
-      .get(API_URL + "/forms/" + this.slug)
+      .get(API_URL + "/forms/" + slug)
       .then((res) => {
-         
+        console.log(res.data.form);
+        this.form = res.data.form;
+        this.questions = res.data.form.questions;
       })
-      .catch((err) => {
-      });
+      .catch((err) => {});
   },
 };
 </script>
