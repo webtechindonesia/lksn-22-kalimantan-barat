fec2330c0d295a0e41ab4595ec43fa7550e97a1c
DasonTio <codingdec14@gmail.com>
2022-10-26 10:26:41 +0700

    SpeedTest

--- SPEEDTEST_MODULE/D2/index.php
+++ SPEEDTEST_MODULE/D2/index.php
@@ -1,7 +1,36 @@
-<?php 
-function categorize($n, $size){
-  
-}
+<?php
+
 $dataFile = file_get_contents(realpath('result.json'));
 $data = json_decode($dataFile);
+$all = [];
+$allStr = [];
+foreach ($data->messages as $message) {
+  if (!isset($all[$message->text])) $all[$message->text] = 1;
+  else $all[$message->text] += 1;
+}
+arsort($all);
+$top5 = array_splice($all, 0, 5);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <ul>
+    <li>Top 5 sent words: <ul>
+        <?php foreach ($top5 as $word => $count) : ?>
+          <li><?= $word ?></li>
+        <?php endforeach; ?>
+      </ul>
+    </li>
+  </ul>
+</body>
+
+</html>
